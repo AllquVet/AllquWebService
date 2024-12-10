8c3793c53334b1ba0161fc94d734a961ee964ab6
AllquVet <allqu6571@gmail.com>
2024-12-10 16:08:08 -0500

    Update allqu.py

--- allqu.py
+++ allqu.py
@@ -52,8 +52,9 @@ def main ():
         #Seleccionar hora comprobar calendar
         st.subheader("Hora")
         #print("Inicio____________")
-
-        hora_actual=str(dt.datetime.now(dt.timezone.utc).time().replace(minute=0,second=0,microsecond=0))[0:5]
+        hora_global=dt.datetime.now(dt.UTC)
+        hora_peru=(hora_global-dt.timedelta(hours=5))
+        hora_actual=str(hora_peru.time().replace(minute=0,second=0,microsecond=0))[0:5]
         horas_service=["10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00"]
         if d==dt.datetime.now(dt.timezone.utc).date():
             start_day=(str(d)+"T"+str(((dt.datetime.now().time()).replace(second=0,microsecond=0))))+"-05:00"
